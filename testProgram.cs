192f4be332329a804502b3d20bd7cb7562d20a52
kinggrant1 <king.grant1@outlook.com>
2024-08-26 18:41:05 -0400

    First commit

---
+++ testProgram.cs
@@ -0,0 +1,9 @@
+public class testProgram() {
+  public void sayHello() {
+    Console.WriteLine("Hello!");
+  }
+
+  public void sayHello(string name) {
+    Console.WriteLine($"Hello {name}!");
+  }
+}
